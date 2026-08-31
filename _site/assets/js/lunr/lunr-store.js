674c9ee9037424dd6f30489605e9e0082ad59566
Jiyeon Jeon <jazzydus@gmail.com>
2026-08-31 09:16:04 +0200

    ㅇ
    ㅇ

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "JJ New Album Release and Concerts",
         "excerpt":"Album: Jiyeon Jeon, Journey Together has been released. 27.12.2025     Listen Now:                Spotify                YouTube Music                Apple Music                Amazon Music      Buy Now:                iTunes                Amazon Music                 bandcamp     ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/album-new-release-and-concerts/",
+        "url": "/album-new-release-and-concerts/",
         "teaser": null
       }]
